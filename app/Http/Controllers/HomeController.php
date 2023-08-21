ab8f0758f9ce522b2f797befcfd2d073e477e720
sitzd <sithumdilshan78@gmail.com>
2023-08-21 20:14:35 +0530

    Display doctors

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,12 +8,16 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 
+use App\Models\Doctor;
+
 class HomeController extends Controller
 {
     public function redirect() { 
         if(Auth::id()) {
             if(Auth::user()->usertype=='0') {
-                return view('user.home');
+
+                $doctor = doctor::all();
+                return view('user.home', compact('doctor'));
             }
             else {
                 return view('admin.home');
@@ -27,6 +31,17 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('user.home');
+        if(Auth::id())
+        {
+            return redirect('home');
+        }
+
+        else
+        {
+        $doctor = doctor::all();
+
+        return view('user.home', compact('doctor'));
+
+        }
     }
 }
